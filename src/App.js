68d87b98e435d307be32acc586f4a2a28d858d96
yjune <yjune0107@gmail.com>
2022-10-01 09:49:57 +0900

    coin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  const [usd, setUsd] = useState();
+  const onChange = (event) => {
+    setUsd(event.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coins! {loading ? "" : `${coins.length}`}</h1>
+
+      {loading ? (
+        ""
+      ) : (
+        <input
+          value={usd}
+          onChange={onChange}
+          id="usd"
+          type="number"
+          placeholder="USD"
+        />
+      )}
+      {loading ? (
+        ""
+      ) : (
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {usd / coin.quotes.USD.price} {coin.name} ({coin.symbol}):
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
